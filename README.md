2c277efb67862fb229a82323fcb3135ffa367070
kjgpta <mailguptakshitij@gmail.com>
2024-05-11 16:31:53 +0530

    prepared pyproject.toml file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Basic Package - Fahrenheit to Kelvin Converter
 
 This is a basic package that provides a simple function to convert temperature from Fahrenheit to Kelvin.
+
+## Installation
+
+To install the package, you can use the following command:
+
+pip install temp-converter
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Basic package for converting temperature units from fahrenheit to kelvin.',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/kjgpta?',
+    url='https://github.com/kjgpta/temp-converter',
     author='kjgpta',
     author_email='myemail@example.com'
 )
